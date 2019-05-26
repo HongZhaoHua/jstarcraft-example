35533576a2439aa3da210f15219f688ea837847b
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-05-26 19:02:29 +0800

    解决Spring-Boot的JPA与核心框架的冲突.

--- src/main/java/com/jstarcraft/example/ExampleApplication.java
+++ src/main/java/com/jstarcraft/example/ExampleApplication.java
@@ -1,12 +1,14 @@
 package com.jstarcraft.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
 //@ImportResource({ "classpath:applicationContext-configuration.xml", "classpath:applicationContext-cache.xml", "classpath:applicationContext-storage.xml" })
-//// 解决Spring-Boot的JPA与核心框架的冲突
-//@EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
+// 解决Spring-Boot的JPA与核心框架的冲突
+@EnableAutoConfiguration(exclude = MongoAutoConfiguration.class)
 public class ExampleApplication {
 
     public static void main(String[] arguments) {
